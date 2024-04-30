b42b58b26500b2150d974ada8505e32a42354e99
Maxyme Bonvent <maxyme.bonvent@hotmail.com>
2024-04-30 12:51:51 +0200

    Création de fichiers models, views, controllers, htaccess et index pour configurer le projet selon l'architecture MVC.

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    // Report all errors
+    error_reporting(E_ALL);
+
+    // Display all errors on the webpage
+    ini_set("display_errors", "on");
+
+    // Database connection
+    require_once("database_connection.php");
+
+    // User ID
+    require_once("get_user_id.php");
+?>
